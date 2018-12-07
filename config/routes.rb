9732ee51f9af737d21000000df3b1a53129ea675
Heidi Engstrom <hengstrom25@gmail.com>
2018-12-07 02:24:25 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
+
   post 'logout', to: 'sessions#destroy'
   resources :users, only: [:new, :create, :show]
+
+  delete 'logout' => 'sessions#destroy'
+  resources :users
   resources :attractions
   resources :rides
   
